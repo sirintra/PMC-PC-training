d132923c8b250bcb36812be704b05e8f97dada20
sirintra <s.nakjang@qub.ac.uk>
2022-11-29 10:26:56 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PMC PanCancerWF training'
+copyright = '2022, Sirintra'
+author = 'Sirintra Nakjang'
 
 release = '0.1'
 version = '0.1.0'
