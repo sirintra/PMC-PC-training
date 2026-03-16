2197c20f7d7dc7981f7cd3de7df76461a69d188b
sirintra <s.nakjang@qub.ac.uk>
2026-03-16 12:29:20 +0000

    Update conf.py
    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,25 @@
+import os
+import sys
 
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "PMC-PC Training"
+author = "Sirintra Nakjang"
+release = "0.1"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
+
+autosummary_generate = True
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+
+html_theme = "sphinx_rtd_theme"
+
+# If your package imports heavy deps, mock them:
+autodoc_mock_imports = ["numpy", "pandas", "scipy", "sklearn"]
